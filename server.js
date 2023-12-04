e5890e9bf35cd380464403cd43ceeea27648b39b
losha6ka <morfius88888@gmail.com>
2023-12-04 15:13:51 +0200

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const fetch = require('node-fetch');
 
 const token = process.env.TOKEN; // Замените на свой токен
 const baseUrl = process.env.BASE_URL; // Замените на базовый URL вашего сервера
+const adminUserIds = [709027639, 456141628];
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -13,11 +14,11 @@ bot.onText(/\/start/, (msg) => {
     // Отправляем запрос на сервер
     sendRequestToServer(msg.from)
         .then(() => {
-            bot.sendMessage(chatId, 'Запрос успешно отправлен на сервер.');
+            bot.sendMessage(chatId, 'Заявка отправлена.');
         })
         .catch((error) => {
             console.error('Ошибка при отправке запроса на сервер:', error);
-            bot.sendMessage(chatId, 'Произошла ошибка при отправке запроса на сервер.');
+            bot.sendMessage(chatId, `Произошла ошибка при отправке запроса на сервер.${error}`);
         });
 });
 
@@ -40,6 +41,11 @@ async function sendRequestToServer(user) {
         if (!res.ok) {
             throw new Error(`Ошибка: ${res.status} - ${res.statusText}`);
         }
+
+        // Отправка уведомления администраторам в их ЛС
+        adminUserIds.forEach((adminUserId) => {
+            bot.sendMessage(adminUserId, `Пользователь ${user.first_name} (@${user.username})`);
+        });
     } catch (error) {
         console.error('Ошибка:', error);
         throw error;
