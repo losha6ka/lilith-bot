ad76ad7570447bf986fd9ca75aa829500deb1b14
losha6ka <morfius88888@gmail.com>
2023-12-04 15:27:35 +0200

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const fetch = require('node-fetch');
 
 const token = process.env.TOKEN; // Замените на свой токен
-const baseUrl = process.env.BASE_URL; // Замените на базовый URL вашего сервера
 const adminUserIds = [709027639, 456141628];
 
 const bot = new TelegramBot(token, { polling: true });
@@ -11,43 +9,25 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
 
-    // Отправляем запрос на сервер
-    sendRequestToServer(msg.from)
+    // Отправляем уведомление администраторам
+    sendNotificationToAdmins(msg.from)
         .then(() => {
             bot.sendMessage(chatId, 'Заявка отправлена.');
         })
         .catch((error) => {
-            console.error('Ошибка при отправке запроса на сервер:', error);
-            bot.sendMessage(chatId, 'Произошла ошибка при отправке запроса на сервер.');
+            console.error('Ошибка при отправке заявки:', error);
+            bot.sendMessage(chatId, 'Произошла ошибка при отправке заявки.');
         });
 });
 
-async function sendRequestToServer(user) {
+async function sendNotificationToAdmins(user) {
     try {
-        let dataSend = {
-            name: user.first_name,
-            telegram: user.username,
-        };
-
-        const res = await fetch(`${baseUrl}/email/sendEmail`, {
-            method: 'POST',
-            body: JSON.stringify(dataSend),
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (!res.ok) {
-            throw new Error(`Ошибка: ${res.status} - ${res.statusText}`);
-        }
-
         // Отправка уведомления администраторам в их ЛС
         adminUserIds.forEach((adminUserId) => {
-            bot.sendMessage(adminUserId, `Пользователь ${user.first_name} (@${user.username})`);
+            bot.sendMessage(adminUserId, `Пользователь ${user.first_name} (@${user.username}).`);
         });
     } catch (error) {
-        console.error('Ошибка:', error);
+        console.error('Ошибка при отправке заявки:', error);
         throw error;
     }
 }
