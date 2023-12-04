3d99e3ab80aee211bc0cfaf524fb7bb9fd01bfdf
losha6ka <morfius88888@gmail.com>
2023-12-04 18:28:52 +0200

    first commit

--- server.js
+++ server.js
@@ -1,85 +1,34 @@
 require('dotenv').config();
-
 const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
-
 const token = process.env.TOKEN; // Замените на свой токен
 const adminUserIds = [709027639, 456141628];
-
 const bot = new TelegramBot(token, { polling: true });
-
 const userStates = {}; // Объект для хранения состояний пользователей
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     const userId = msg.from.id;
 
-    // Установим начальное состояние пользователя
-    userStates[userId] = {
-        step: 1, // текущий этап
-        data: {} // объект для хранения данных пользователя
-    };
+    // Проверяем, есть ли у пользователя контекст, если нет - создаем
+    const context = userStates[userId] || { step: 0, data: {}, completed: false };
+    userStates[userId] = context;
 
-    // Начинаем цепочку действий
-    bot.sendMessage(chatId, 'Привет! Как к вам обращаться?');
-});
+    switch (context.step) {
+        case 0:
+            bot.sendMessage(chatId, 'Привет! Как к вам обращаться?');
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const userId = msg.from.id;
-    const userState = userStates[userId];
+            // Ожидаем ответа на первый вопрос
+            bot.once('message', (msg) => {
+                if (context && context.step === 0) {
+                    context.data.name = msg.text;
+                    context.step++;
 
-    switch (userState.step) {
-        case 1:
-            // Сохраняем имя пользователя и переходим к следующему этапу
-            userState.data.name = msg.text;
-            userState.step++;
-            bot.sendMessage(chatId, `Отлично, ${userState.data.name}! Теперь введите ваш возраст.`);
-            break;
+                    bot.sendMessage(chatId, `Отлично, ${context.data.name}! Теперь введите ваш возраст.`);
 
-        case 2:
-            // Проверяем возраст пользователя
-            const age = parseInt(msg.text, 10);
-            if (isNaN(age) || age < 0) {
-                bot.sendMessage(chatId, 'Пожалуйста, введите корректный возраст.');
-            } else if (age < 18) {
-                bot.sendMessage(chatId, 'Извините, но мы работаем только с совершеннолетними.');
-                delete userStates[userId]; // Завершаем цепочку для текущего пользователя
-            } else {
-                userState.data.age = age;
-                userState.step++;
-                bot.sendMessage(chatId, 'Есть ли у вас опыт работы в OnlyFans? (да/нет)');
-            }
-            break;
-
-        case 3:
-            // Проверяем ответ на наличие опыта работы в OnlyFans
-            const hasExperience = msg.text.toLowerCase();
-            if (hasExperience === 'да' || hasExperience === 'нет') {
-                userState.data.hasExperience = hasExperience;
-                userState.step++;
-                bot.sendMessage(chatId, 'Теперь отправьте ваше фото.');
-            } else {
-                bot.sendMessage(chatId, 'Пожалуйста, ответьте "да" или "нет".');
-            }
-            break;
-
-        case 4:
-            // Проверяем, что пришло фото
-            if (msg.photo && msg.photo.length > 0) {
-                // Получаем информацию о фото
-                const photo = msg.photo[0];
-                const fileId = photo.file_id;
-
-                // Сохраняем информацию о фото и завершаем цепочку
-                userState.data.photoFileId = fileId;
-                userState.step++;
-                bot.sendMessage(chatId, 'Ваша заявка отправлена.');
-                sendNotificationToAdmins(userId, userState.data);
-                delete userStates[userId];
-            } else {
-                bot.sendMessage(chatId, 'Пожалуйста, отправьте фото.');
-            }
+                    // Ожидаем ответа на второй вопрос
+                    bot.once('message', handleAgeInput);
+                }
+            });
             break;
 
         default:
@@ -87,6 +36,87 @@ bot.on('message', (msg) => {
             break;
     }
 });
+
+// Добавьте обработку других этапов по мере необходимости
+
+function handleAgeInput(msg) {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const context = userStates[userId] || { step: 0, data: {}, completed: false };
+
+    const age = parseInt(msg.text, 10);
+
+    if (isNaN(age) || age < 0) {
+        bot.sendMessage(chatId, 'Пожалуйста, введите корректный возраст.');
+
+        // Возвращаемся на предыдущий шаг
+        context.step--;
+
+        // Ожидаем ответа на текущий вопрос
+        bot.once('message', handleAgeInput);
+    } else if (age < 18) {
+        bot.sendMessage(chatId, 'Извините, но мы работаем только с совершеннолетними.');
+        context.completed = true;
+    } else {
+        context.data.age = age;
+        context.step++;
+        bot.sendMessage(chatId, 'Есть ли у вас опыт работы в OnlyFans? (да/нет)');
+
+        // Ожидаем ответа на третий вопрос
+        bot.once('message', handleExperienceInput);
+    }
+}
+
+function handleExperienceInput(msg) {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const context = userStates[userId] || { step: 0, data: {}, completed: false };
+
+    // Обрабатываем ответ пользователя
+    const hasExperience = msg.text.toLowerCase();
+
+    if (['да', 'нет'].includes(hasExperience)) {
+        context.data.hasExperience = hasExperience;
+        context.step++;
+        bot.sendMessage(chatId, 'Теперь отправьте ваше фото.');
+
+        // Ожидаем ответа на четвертый вопрос
+        bot.once('message', handlePhotoInput);
+    } else {
+        bot.sendMessage(chatId, 'Пожалуйста, ответьте "да" или "нет".');
+
+        // Возвращаемся на предыдущий шаг
+        context.step--;
+
+        // Ожидаем ответа на текущий вопрос
+        bot.once('message', handleExperienceInput);
+    }
+}
+
+function handlePhotoInput(msg) {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const context = userStates[userId] || { step: 0, data: {}, completed: false };
+
+    // Обрабатываем ответ пользователя
+    if (msg.photo && msg.photo.length > 0) {
+        const photo = msg.photo[0];
+        const fileId = photo.file_id;
+        context.data.photoFileId = fileId;
+        context.step++;
+        bot.sendMessage(chatId, 'Ваша заявка отправлена.');
+        sendNotificationToAdmins(userId, context.data);
+        context.completed = true;
+    } else {
+        bot.sendMessage(chatId, 'Пожалуйста, отправьте фото.');
+
+        // Возвращаемся на предыдущий шаг
+        context.step--;
+
+        // Ожидаем ответа на текущий вопрос
+        bot.once('message', handlePhotoInput);
+    }
+}
 
 function sendNotificationToAdmins(userId, userData) {
     try {
